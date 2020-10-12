77b0fc715ab4c1166c6918a63d54ea628d084c76
Jason <jdale215@gmail.com>
2020-10-12 17:54:55 -0400

    correct project deleted

--- pkg/entity/project.go
+++ pkg/entity/project.go
@@ -1,11 +1,12 @@
 package entity
 
 // If you want historical data, leave it.
-// If you want phone number update, for eample, use a contact id
+// If you want phone number update, for example, use a contact id
 // Project is a construction project
 type Project struct {
 	ID                  string `json:"id"`
 	Created             string `json:"created"`
+	Deleted             bool   `json:"deleted"`
 	Name                string `json:"name"`
 	StartDate           string `json:"startDate"`
 	DueDate             string `json:"dueDate"`
@@ -31,6 +32,7 @@ type Project struct {
 // UpdateProjectRequest is a construction project
 type UpdateProjectRequest struct {
 	Name                string `json:"name"`
+	Deleted             bool   `json:"deleted"`
 	StartDate           string `json:"startDate"`
 	DueDate             string `json:"dueDate"`
 	Address             string `json:"address"`
